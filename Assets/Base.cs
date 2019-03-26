f8a10dfe0463987fbdd955c0e423c59c491f0a6a
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2019-03-26 10:45:28 +0000

    Complete

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -10,14 +10,22 @@ public class Base : MonoBehaviour
     public TextMeshPro text;
 
     public GameObject fighterPrefab;
+    
+    List<Fighter> Fighters = new List<Fighter>();
+
+
+    public Color BaseColour;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Renderer r in GetComponentsInChildren<Renderer>())
-        {
-            r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+        BaseColour = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+
+        foreach (Renderer r in GetComponentsInChildren<Renderer>()) {
+            r.material.color = BaseColour;
+
+            StartCoroutine(GainT());
         }
     }
 
@@ -25,5 +33,65 @@ public class Base : MonoBehaviour
     void Update()
     {
         text.text = "" + tiberium;
+        
+        decision();
+        
     }
+
+
+    void decision() {
+
+
+        if (tiberium >= 10f) {
+
+           GameObject temp =  Instantiate(fighterPrefab);
+            temp.transform.position = transform.position;
+           // temp.get
+
+            foreach (Renderer r in temp.GetComponentsInChildren<Renderer>()) {
+                r.material.color = BaseColour;
+            }
+
+            temp.GetComponent<Fighter>().HomeBase = this.transform; 
+            
+            Fighters.Add(temp.GetComponent<Fighter>());
+
+
+            tiberium -= 10f;
+
+        }
+
+
+
+    }
+    
+    
+
+
+
+    IEnumerator GainT() {
+
+        while (true) {
+            yield return new WaitForSeconds(1f);
+
+            tiberium++;
+        }
+
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("bullet")) {
+
+
+            tiberium -= 0.5f;
+            
+            Destroy(other.gameObject,0.5f);
+
+        }
+    }
+
+
 }
---
+++ Assets/Fighter.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class Fighter : MonoBehaviour {
+
+
+    public float Tiberium;
+
+    public Transform HomeBase;
+
+    public float AttackRange;
+
+
+    private Transform TargetBase;
+
+    public State currentState = State.seeking;
+
+    public GameObject BulletPrefab;
+
+    public enum State { 
+    
+    seeking,
+    attacking,
+    retreating,
+    refuling
+    
+    }
+
+    private void Start() {
+
+        Base[] Bases = FindObjectsOfType<Base>();
+
+        while (TargetBase == null) {
+
+
+            int choice = Random.Range(0, Bases.Length);
+
+            Base check = Bases[choice];
+
+            if (check != HomeBase.GetComponent<Base>()) {
+
+                TargetBase = check.transform;
+
+            }
+
+
+        }
+
+
+        GetComponent<Arrive>().targetPosition = TargetBase.position; 
+
+
+    }
+
+
+    void Update() {
+
+        switch (currentState) {
+                
+            
+            case(State.seeking):
+
+
+                if (Vector3.Distance(transform.position, TargetBase.position) <= AttackRange) {
+
+                    currentState = State.attacking;
+
+                    //GetComponent<Arrive>().enabled = false;
+
+                    StartCoroutine(Shoot());
+
+
+                }
+                
+
+                break;
+
+
+            case (State.attacking) :
+
+
+                if (Tiberium <= 0) {
+
+                    currentState = State.retreating;
+
+                  //  GetComponent<Arrive>().enabled = true;
+                    GetComponent<Arrive>().targetPosition = HomeBase.position;
+
+
+
+                }
+
+//Debug.Log("I am attacking");
+
+                break;
+            
+            case (State.retreating):
+
+                if (Vector3.Distance(transform.position, HomeBase.position) < 10f && HomeBase.GetComponent<Base>().tiberium >= 7f) {
+
+                    currentState = State.refuling;
+
+
+                }
+
+                break;
+                
+
+            case (State.refuling):
+
+
+                currentState = State.seeking;
+
+                GetComponent<Arrive>().targetPosition = TargetBase.position;
+
+                HomeBase.GetComponent<Base>().tiberium -= 7f;
+
+                Tiberium = 7;
+                
+
+                break;
+            
+            
+            
+            
+        }
+        
+        
+       
+
+
+
+    }
+
+
+
+    IEnumerator Shoot() {
+
+
+        while (Tiberium > 0) {
+
+
+            GameObject tempBullet = Instantiate(BulletPrefab);
+            tempBullet.transform.position = transform.position;
+            tempBullet.transform.rotation = transform.rotation;
+
+            tempBullet.GetComponent<MeshRenderer>().material.color = HomeBase.GetComponent<Base>().BaseColour;//GetComponent<MeshRenderer>().material.color;
+
+            Tiberium--;
+            
+            
+            yield return new WaitForSeconds(0.2f);
+
+
+        }
+
+
+    }
+
+
+}
